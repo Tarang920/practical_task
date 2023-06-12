035b0358aa08774e04be5a9c4cd13fca3508c97c
developer4 <developer4@odlesinc.com>
2023-06-12 17:14:36 +0530

    complete CRUD operation of User

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Resources\GeneralResponse;
+use App\Http\Resources\GeneralError;
+use Illuminate\Support\Facades\Validator;
+use Log,DB;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function createUser(Request $request)
+    { 
+        DB::beginTransaction();
+        try{
+            $data = $request->all();
+            
+            // Validation for the Create new user
+            $validator = Validator::make($data,[
+                'first_name' => ['required','max:255'],
+                'last_name' => ['required','max:255'],
+                'DOB' => ['required'],
+                'gender' => ['required',]
+            ]);
+
+            if($validator->fails())
+            {
+                return new GeneralError(['message' => $validator->messages(),'toast' => true]);
+            }else{
+                $newUser = User::create($data);
+                DB::commit();
+                return new GeneralResponse(['data'=> $newUser,'message' => 'New User add successfully', 'toast' => true]);
+            }            
+        }catch(\Exception $e){
+            \Log::info('Message:: '.$e->getMessage().' line:: '.$e->getLine().' Code:: '.$e->getCode().' file:: '.$e->getFile());
+            DB::rollback();
+            return redirect()->back()->withErrors(['msg' => 'Something Went Wrong!']);
+        }
+    }
+
+    public function getUserDetail(Request $request)
+    {
+        $data = $request->all();
+        $validator = Validator::make($data,['id'=>'required|exists:users,id']);
+
+        if($validator->fails())
+        {
+            return new GeneralError(['message' => $validator->messages(),'toast' => true]);
+        }else{
+            $userDetail = User::where('id',$data['id'])->get();
+            return new GeneralResponse(['data'=> $userDetail,'message' => 'User Detail Fetch successfully', 'toast' => true]);
+        }
+    }
+
+    public function editUserDetail(Request $request)
+    {
+        $data = $request->all();
+        $validator = Validator::make($data,['id'=>'required|exists:users,id']);
+
+        if($validator->fails())
+        {
+            return new GeneralError(['message' => $validator->messages(),'toast' => true]);
+        }else{
+            $updateUser = User::where('id',$data['id'])->update($data);
+
+            // Query for after updating the value to retrive updated record for append in response
+            $latestUserDetails = User::where('id',$data['id'])->get();
+
+            return new GeneralResponse(['data'=> $latestUserDetails,'message' => 'User Detail Updated successfully', 'toast' => true]);
+        }
+    }
+
+    public function deleteUser(Request $request)
+    {
+        $data = $request->all();
+
+        $validator = Validator::make($data,['id'=>'required|exists:users,id']);
+
+        if($validator->fails())
+        {
+            return new GeneralError(['message' => $validator->messages(),'toast' => true]);
+        }else{
+            $deleteUser = User::find($data['id']);
+            $deleteUser->delete();
+            return new GeneralResponse(['data' => $deleteUser,'message' => 'User Deleted Successfully']);
+
+        }
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+/* User API's */
+Route::group(['prefix' => 'User', 'namespace' => 'User'], function () {
+    Route::post('/create',[UserController::class,'createUser']);
+    Route::post('/detail',[UserController::class,'getUserDetail']);
+    Route::post('/update',[UserController::class,'editUserDetail']);
+    Route::post('/delete',[UserController::class,'deleteUser']);
 });
